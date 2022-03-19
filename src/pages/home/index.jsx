046ac99bb6978c98136ce66db24fd4f82b0c3160
panjinquan <panjinquan@clickwifi.net>
2022-03-19 11:54:05 +0800

    feat:添加动态路由

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import './index.less'; // 引入样式文件
 
-import './index.less';  // 引入样式文件
 
 
 class Home extends Component {
 
   jump = () => {
-    console.log('跳转')
     this.props.history.push('/login')
+  }
+
+  toke = ()=>{
+    localStorage.setItem("token",123);
+    console.log('登陆')
+    this.jump()
+  }
+
+  exit =()=>{
+    localStorage.removeItem("token");
+    console.log('退出')
   }
 
   render() {
@@ -15,6 +25,8 @@ class Home extends Component {
       <div className="main">
         <div>我是首页2</div>
         <div onClick={this.jump}>跳转</div>
+        <div onClick={this.toke}>登陆</div>
+        <div onClick={this.exit}>退出</div>
       </div>
     );
   }
--- src/pages/router/index.jsx
+++ src/pages/router/index.jsx
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
-import { Route,HashRouter } from 'react-router-dom';
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Home from '../home/index.jsx';
 import Login from '../login/index.jsx';
 
 class App extends Component {
+  state = {
+    location: {
+        pathname: window.location.hash.slice(1), // #/user
+        state:window.history.state
+    }
+  }
+  //组件挂载完成之后，根据hash改变pathname的值
+  componentDidMount(){
+    let that = this
+    window.addEventListener('hashchange',event =>{
+      that.setState({
+        ...that.state,
+        location: {
+            ...that.state.location,
+            pathname:window.location.hash.slice(1) 
+        }
+      })
+      console.log('跳转',window.location.hash || '/')
+    })
+  }
   render() {
+    const token = localStorage.getItem("token")
+    console.log({token})
     return (
       <HashRouter>
+        <Switch>
           <Route path='/' exact component={Home}></Route>
-          <Route path='/login' exact component={Login}></Route>
+          {
+            token ?
+            (<Route path='/login' exact component={Login}></Route>) :
+             null
+          }
+          <Redirect from="*" to='/' />
+        </Switch>
       </HashRouter>
     )
   }
